39f8c59d177157ba80526f6d0203b259419f7078
Linesh Mehta <lineshmehta@gmail.com>
2018-06-15 15:27:07 -0500

    Correct Project client

--- components/allocations/src/main/java/io/pivotal/pal/tracker/allocations/ProjectClient.java
+++ components/allocations/src/main/java/io/pivotal/pal/tracker/allocations/ProjectClient.java
@@ -10,20 +10,23 @@ import java.util.concurrent.ConcurrentHashMap;
 
 public class ProjectClient {
 
-    private final RestOperations restOperations;
-    private final String registrationServerEndpoint;
-
     private final Logger logger = LoggerFactory.getLogger(getClass());
     private final Map<Long, ProjectInfo> projectsCache = new ConcurrentHashMap<>();
+    private final RestOperations restOperations;
+    private final String endpoint;
 
     public ProjectClient(RestOperations restOperations, String registrationServerEndpoint) {
-        this.restOperations= restOperations;
-        this.registrationServerEndpoint = registrationServerEndpoint;
+        this.restOperations = restOperations;
+        this.endpoint = registrationServerEndpoint;
     }
 
     @HystrixCommand(fallbackMethod = "getProjectFromCache")
     public ProjectInfo getProject(long projectId) {
-        return restOperations.getForObject(registrationServerEndpoint + "/projects/" + projectId, ProjectInfo.class);
+        ProjectInfo project = restOperations.getForObject(endpoint + "/projects/" + projectId, ProjectInfo.class);
+
+        projectsCache.put(projectId, project);
+
+        return project;
     }
 
     public ProjectInfo getProjectFromCache(long projectId) {
